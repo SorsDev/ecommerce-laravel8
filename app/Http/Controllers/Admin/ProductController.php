19f6643abde47e6ba224a1cef7ad8bb5b1f4ba58
SorsDev <sorsromerze@gmail.com>
2021-05-20 16:14:44 -0500

    moduloProducto

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
 
     public function getHome()
     {
-        $products = Product::orderBy('id','desc')->paginate(25);
+        $products = Product::with(['cat'])->orderBy('id','desc')->paginate(25);
         $data = ['products' => $products ];
     	return view('admin.products.home', $data);
     }
@@ -85,12 +85,67 @@ class ProductController extends Controller
         endif;
     }
 
+
     public function getProductEdit($id)
     {
         $p = Product::findOrFail($id);
         $cats = Category::where('module','0')->pluck('name','id');
         $data = ['cats' => $cats, 'p' => $p];
         return view('admin.products.edit', $data);
+    }
+
+
+    public function postProductEdit($id, Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'price' => 'required',
+            'content' => 'required',
+        ]; 
+
+        $messages = [
+            'name.required' => 'El nombre del producto es requerido',
+            'img.image' => 'El archivo no es una imagen',
+            'price.required' => 'Ingrese el precio del producto',
+            'content.required' => 'Ingrese una descripción del producto',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if($validator->fails()):
+            return back()->withErrors($validator)->with('message','Se ha producido un error.')->with('typealert','danger')->withInput();
+        else:
+            $product =Product::find($id);
+            $product->status = $request->input('status');
+            $product->name = e($request->input('name'));
+            $product->category_id = $request->input('category');
+            if($request->hasFile('img')):
+                $path = '/'.date('Y-m-d'); //2021-02-06
+                $fileExt = trim($request->file('img')->getClientOriginalExtension());
+                $upload_path = Config::get('filesystems.disks.uploads.root');
+                $name = Str::slug(str_replace($fileExt, '', $request->file('img')->getClientOriginalName()));
+
+                $filename = rand(1,999).'-'.$name.'.'.$fileExt;
+                $file_file = $upload_path.'/'.$path.'/'.$filename;
+                $product->file_path = date('Y-m-d');
+                $product->image = $filename;
+            endif;
+            $product->price = $request->input('price');
+            $product->in_discount = $request->input('indiscount');
+            $product->discount = $request->input('discount');
+            $product->content = e($request->input('content'));
+
+            if($product->save()):
+                if($request->hasFile('img')):
+                    $fl = $request->img->storeAs($path,$filename,'uploads');
+                    $img = Image::make($file_file);
+                    $img->fit(256,256, function($constraint){
+                        $constraint->upsize();
+                    });
+                    $img->save($upload_path.'/'.$path.'/t_'.$filename);
+                endif;
+                return back()->with('message','Actualizado con éxito.')->with('typealert','success');
+            endif;
+        endif;
     }
 
 
